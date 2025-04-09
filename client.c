a55ae0280b9368ddbcdea055b0d9651ee2859e27
kshotel2 <51342470+kshotel2@users.noreply.github.com>
2025-04-09 23:42:14 +0900

    Update client.c
    정리

--- client.c
+++ client.c
@@ -1,4 +1,4 @@
-// tcp_client.c
+//client
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,29 +14,24 @@ int main() {
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
 
-    // 1. 소켓 생성
+    //create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        perror("socket");
-        exit(1);
-    }
+    if(sockfd == -1){perror("socket"); exit(1);}
 
-    // 2. 서버 주소 설정
+    //server setting
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
 	inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
 
-    // 3. 서버에 연결
-    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("connect");
-        close(sockfd);
-        exit(1);
-    }
-    printf("서버에 연결됨\n");
+    //connect
+    if(connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
+        perror("connect"); close(sockfd); exit(1);}
+	
+    printf("Connected to Server\n");
 
-    // 4. 데이터 송수신
-    while (1) {
+    //communication
+    while(1){
         printf("메시지 입력 (종료: exit): ");
         fgets(buffer, BUFFER_SIZE, stdin);
         if (strncmp(buffer, "exit", 4) == 0) break;
@@ -46,8 +41,6 @@ int main() {
         read(sockfd, buffer, BUFFER_SIZE - 1);
         printf("서버 응답: %s\n", buffer);
     }
-
-    // 5. 종료
     close(sockfd);
     return 0;
 }
