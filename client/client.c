7aed7437581acd594bc2141639efa9b23f3afa64
choi0930 <0930alan@gmail.com>
2025-04-10 13:13:33 +0900

    client -> server file 전송 추가

---
+++ client/client.c
@@ -0,0 +1,113 @@
+// tcp_client.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 12345
+#define BUFFER_SIZE 1024
+#define MAXLINE 256
+int main() {
+    struct stat obj;
+    int sockfd, fd, file_size, status;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE], filename[MAXLINE], buf[BUFFER_SIZE], file_buf[BUFFER_SIZE];
+
+    // 1. 소켓 생성
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    // 2. 서버 주소 설정
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+	inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+
+    // 3. 서버에 연결
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("connect");
+        close(sockfd);
+        exit(1);
+    }
+    printf("서버에 연결됨\n");
+
+    // 4. 데이터 송수신
+    while (1) {
+        printf("명령어 입력 [put, exit](종료: exit): ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;  
+
+        if (strncmp(buffer, "exit", 4) == 0) break;
+        
+        else if(strncmp(buffer, "put", 3) == 0){ //put 명령어
+            
+            printf("업로드 할 파일명을 입력해주세요 :");
+            if (fgets(filename, sizeof(filename), stdin) == NULL) {
+                printf("입력 오류!\n");
+                continue;
+            }
+            filename[strcspn(filename, "\n")] = 0;  // 엔터 제거
+            
+            printf("filename: %s\n", filename);
+            if (strlen(filename) == 0) {
+                printf("파일명이 비어있습니다. 다시 입력해주세요.\n");
+                continue;
+            }
+            
+            if((fd = open(filename, O_RDONLY)) == -1){//파일 open
+                printf("파일이 없습니다.\n");
+                continue;
+            };
+
+            strcpy(buf, "put ");
+            strcat(buf, filename);
+            
+            printf("명령어 조합 완료: %s\n", buf); 
+            printf("명령어 전송 시작\n");
+            send(sockfd, buf, BUFFER_SIZE, 0);//명령어 전송
+            
+            //파일 크기 
+            
+            stat(filename, &obj);
+            file_size = obj.st_size;
+            printf("파일 크기 : %d\n", file_size);
+
+            send(sockfd, &file_size, sizeof(int), 0); //파일 크기 전송
+
+            read(fd, file_buf, file_size);
+            send(sockfd, file_buf, file_size, 0); //파일 전송
+            
+            recv(sockfd, &status, sizeof(int), 0);
+            if(status){//업로드 성공여부 판단
+                printf("업로드 완료\n");
+            }else{
+                printf("업로드 실패\n");
+            }
+
+            close(fd);
+        }//end put
+            
+
+        /*
+        write(sockfd, buffer, strlen(buffer));
+        memset(buffer, 0, BUFFER_SIZE);
+        read(sockfd, buffer, BUFFER_SIZE - 1);
+        printf("서버 응답: %s\n", buffer);
+        */
+    }
+
+    // 5. 종료
+    close(sockfd);
+    return 0;
+ 
+}
+	
---
+++ server/server.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+#define SERVER_IP "127.0.0.1"
+int main() {
+    int server_fd, client_fd, fd, file_size;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len;
+	char buffer[BUFFER_SIZE], command[5], filename[256], buf[BUFFER_SIZE];
+
+  	// 1. 소켓 생성
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {perror("socket"); exit(1);}
+
+    // 2. 서버 주소 설정
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET; 				
+    //inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+   server_addr.sin_addr.s_addr = INADDR_ANY;		
+    server_addr.sin_port = htons(PORT);
+
+    // 3. 바인드
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind");
+        close(server_fd);
+        exit(1);
+    }
+	while(1){
+    	// 4. 리슨
+    	if (listen(server_fd, 5) == -1) {
+    	    perror("listen");
+    	    close(server_fd);
+        	exit(1);
+    	}
+    	printf("서버 대기 중...\n");
+
+    	// 5. 연결 수락
+    	client_len = sizeof(client_addr);
+    	client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+    	if (client_fd == -1) {
+    	    perror("accept");
+    	    close(server_fd);
+    	    exit(1);
+    	}
+    	printf("클라이언트 연결됨\n");
+
+		while(1){
+
+			memset(buffer, 0, BUFFER_SIZE);
+			printf("명령 대기 중...\n");
+		
+			int recv_len = recv(client_fd, buffer, BUFFER_SIZE, 0);
+			if (recv_len <= 0) {
+				perror("recv 실패");
+				break;
+			}
+	
+			sscanf(buffer, "%s", command);
+
+			printf("클라이언트로부터 명령 수신: %s\n", command);
+
+			if(!strcmp(command, "put")){
+				int check = 0;
+				char *file_data;
+
+				sscanf(buffer + strlen(command), "%s", filename);
+				printf("filename: %s\n", filename);
+				while(1){
+					fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
+					if(fd == -1)
+						sprintf(filename + strlen(filename), "_1");
+					else
+						break;
+				}
+
+				recv(client_fd, &file_size, sizeof(int), 0);
+				file_data = malloc(file_size);
+
+				recv(client_fd, buf, file_size, 0);
+				check = write(fd, buf, file_size);
+				printf("%s save success\n", filename);
+				close(fd);
+				free(file_data);
+
+				send(client_fd, &check, sizeof(int), 0);
+			}
+			
+			/*
+        	int bytes_read = read(client_fd, buffer, BUFFER_SIZE - 1);
+			if (bytes_read <= 0) break;
+        	printf("수신: %s\n", buffer);
+        	// 클라이언트에게 그대로 되돌려보냄 (에코)
+        	write(client_fd, buffer, bytes_read);
+			*/
+			
+
+    	}	
+	}
+
+    // 7. 종료
+    close(client_fd);
+    close(server_fd);
+    return 0;
+}
